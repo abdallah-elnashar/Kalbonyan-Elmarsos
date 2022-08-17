526c55330f6e1cee29f5f5306062f1f8b52c0749
abdallah-elnashar <90924885+abdallah-elnashar@users.noreply.github.com>
2022-08-18 00:52:02 +0200

    Add files via upload

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
@@ -1,1 +1,15 @@
+Food = input("What's your favourite food? ")
 
+if Food == "cookies":
+    print("yep so amazing!!!")
+else:
+    print("yuck that's not it")
+
+print("Thanks for saying")
+
+  
+
+ 
+ 
+
+ 
