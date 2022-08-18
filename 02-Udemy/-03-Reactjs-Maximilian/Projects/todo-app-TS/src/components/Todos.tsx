641f74623c337352e011ffa6817804e0dce634c9
abdallah-elnashar <90924885+abdallah-elnashar@users.noreply.github.com>
2022-08-18 19:04:36 +0200

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/todo-app-TS/src/components/Todos.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+
+import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
+import { TodosContext } from "../store/todos-context";
+
+const Todos: React.FC = () => {
+  const todoCtx = useContext(TodosContext);
+
+  return (
+    <ul className={classes.todos}>
+      {todoCtx.items.map((item) => {
+        return (
+          <TodoItem
+            key={item.id}
+            text={item.text}
+            onRemoveTodo={todoCtx.removeTodo.bind(null, item.id)}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Todos;
