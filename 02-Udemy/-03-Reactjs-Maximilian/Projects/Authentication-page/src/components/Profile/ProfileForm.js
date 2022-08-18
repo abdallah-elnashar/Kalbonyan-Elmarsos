4a29490dd21d596780d2221f143890b190b9bca3
abdallah-elnashar <90924885+abdallah-elnashar@users.noreply.github.com>
2022-08-18 19:00:53 +0200

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/Authentication-page/src/components/Profile/ProfileForm.js
@@ -0,0 +1,51 @@
+import { useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
+import classes from "./ProfileForm.module.css";
+
+const ProfileForm = () => {
+  const history = useHistory();
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAt8if4UwCE9H18QClUCMgs1VmH8_D2pMA",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      history.replace("/");
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          ref={newPasswordInputRef}
+          minLength="7"
+        />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;
