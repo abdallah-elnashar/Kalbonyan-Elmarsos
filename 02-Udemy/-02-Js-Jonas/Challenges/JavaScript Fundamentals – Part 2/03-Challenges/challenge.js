4b3a9b57b64c4a4b562e20721eb07d8af6926482
abdallah-elnashar <90924885+abdallah-elnashar@users.noreply.github.com>
2022-09-16 18:04:08 +0200

    Create challenge.js

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 2/03-Challenges/challenge.js
@@ -0,0 +1,16 @@
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+  },
+};
+const Mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+  },
+};
