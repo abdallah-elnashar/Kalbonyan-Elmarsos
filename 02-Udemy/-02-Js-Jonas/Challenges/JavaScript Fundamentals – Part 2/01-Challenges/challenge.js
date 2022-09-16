16a671c390bb433c0a919681268629f647c710dc
abdallah-elnashar <90924885+abdallah-elnashar@users.noreply.github.com>
2022-09-16 18:03:05 +0200

    Update challenge.js

--- 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 2/01-Challenges/challenge.js
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 2/01-Challenges/challenge.js
@@ -1,3 +1,13 @@
 const calcAverage = (a, b, c) => {
   return (a + b + c) / 3;
 };
+
+const dolphinsAvg = calcAverage(44, 23, 71);
+const koalasAvg = calcAverage(65, 54, 49);
+
+const checkWinner = (koalasAvg, dolphinsAvg) => {
+  if (koalasAvg >= 2 * dolphinsAvg) {
+  } else {
+    console.log(`Dolpins win (${dolphinsAvg} vs. ${koalasAvg})`);
+  }
+};
